d96a9b8a22b7d575187d78008d5008dc7124f687
nitro41992 <nick.k@iton.net>
2020-04-17 01:41:04 -0400

    Task: added Update Task Name. Refactored some code, added validations

--- main-prompt.py
+++ main-prompt.py
@@ -29,7 +29,7 @@ def select_column(list, column):
 
 command_completer = WordCompleter(
 	['add_task', 'delete_task', 'end_task', 'list_pending_tasks', 'list_all_tasks', 
-	'pause_task', 'restart_task', 'update_project_name', 'exit'], 
+	'pause_task', 'restart_task', 'update_task_name', 'exported_completed_tasks', 'exit'], 
 	ignore_case=True)
 
 while 1:
@@ -65,15 +65,20 @@ while 1:
 			completer=project_command_completer
 		)
 
+
 		start_time = get_timestamp()
+		existing_task_names = select_column(task_table.search(where('project_name') == task_project), 'task_name')
 
-		task_table.insert({'task_name': task_name, 'project_name': task_project, 'start_date': start_time, 'end_date': '',
-		 'last_restart_date': start_time, 'last_paused_date': '', 'paused': False, 'duration': '0 days, 0:00:00'})
-		
-		if task_project not in project_list:
-			project_table.insert({'project_name': task_project, 'created_on': start_time})
+		if task_name not in existing_task_names:
+			task_table.insert({'task_name': task_name, 'project_name': task_project, 'start_date': start_time, 'end_date': '',
+			'last_restart_date': start_time, 'last_paused_date': '', 'paused': False, 'duration': '0 days, 0:00:00'})
 
-		click.echo(f'Task: "{task_name}" successfully started. Time: {start_time}')
+			if task_project not in project_list:
+				project_table.insert({'project_name': task_project, 'created_on': start_time})
+
+			click.echo(f'Task: "{task_name}" successfully started. Time: {start_time}')
+		else:
+			click.echo('That Task name already exists for that project. Please choose a different Task name')
 
 	elif user_input == 'end_task':
 		task_list = select_column(task_table.search(where('end_date') == ''), 'task_name')
@@ -86,27 +91,32 @@ while 1:
 			completer = task_command_completer,
 		)
 
-		task_end_time = get_timestamp()
-
-		diff = 0
-	
-		try:
-			current_start_time = select_column(task_table.search(where('task_name') == task_to_end), 'start_date')[0]
-			current_task_project = select_column(task_table.search(where('task_name') == task_to_end), 'project_name')[0]
-
-			formatted_end_date = datetime.strptime(task_end_time, format_str)
-			formatted_start_date = datetime.strptime(current_start_time, format_str)
-		
-
-			diff = str(formatted_end_date - formatted_start_date)
-		except:
-			pass
+		current_time = get_timestamp()
 
 		if task_to_end in task_list:
-			task_table.update({'end_date': task_end_time, 'duration': diff}, (where('task_name') == task_to_end) & (where('project_name') == current_task_project))
-			click.echo(f'Task: "{task_to_end}" successfully completed. Time: {task_end_time}')
+			current_start_time = select_column(task_table.search(where('task_name') == task_to_end), 'last_restart_date')[0]
+			current_task_project = select_column(task_table.search(where('task_name') == task_to_end), 'project_name')[0]
+			current_duration = select_column(task_table.search(where('task_name') == task_to_end), 'duration')[0]
+
+			formatted_current_time = datetime.strptime(current_time, format_str)
+			formatted_start_date = datetime.strptime(current_start_time, format_str)
+
+			if 'days' in current_duration:
+				days_v_hms = current_duration.split('days,')
+				hms = days_v_hms[1].split(':')
+			else:
+				hms = current_duration.split(':')
+
+			dt = timedelta(hours=int(hms[0]), minutes=int(hms[1]), seconds=float(hms[2]))
+
+			diff = formatted_current_time - formatted_start_date
+			paused_duration = str(dt + diff)
+		
+			task_table.update({'duration': paused_duration, 'end_date': current_time}, (where('task_name') == task_to_end) & (where('project_name') == current_task_project))
+			click.echo(f'Task: "{task_to_end}" successfully ended. Time: {current_time}')
+
 		else:
-			click.echo('That Task does not exist, please try again.')
+			click.echo('That Task does not exist or has ended, please try again.')
 	
 	elif user_input == 'delete_task':
 		task_list = select_column(task_table.all(), 'task_name')
@@ -115,7 +125,7 @@ while 1:
 		task_session = PromptSession()
 
 		task_to_delete = task_session.prompt(
-			'Select Started Task to Delete: ',
+			'Select Task to Delete: ',
 			completer = task_command_completer,
 		)
 
@@ -183,7 +193,7 @@ while 1:
 			formatted_current_time = datetime.strptime(current_time, format_str)
 			formatted_start_date = datetime.strptime(current_start_time, format_str)
 			
-			if 'date' in current_duration:
+			if 'days' in current_duration:
 				days_v_hms = current_duration.split('days,')
 				hms = days_v_hms[1].split(':')
 			else:
@@ -207,22 +217,50 @@ while 1:
 		task_session = PromptSession()
 
 		task_to_restart = task_session.prompt(
-			'Select Started Task to Restart: ',
+			'Select Paused Task to Restart: ',
 			completer = task_command_completer,
 		)
 
 		current_time = get_timestamp()
 
-		try:
-			current_task_project = select_column(task_table.search(where('task_name') == task_to_restart), 'project_name')[0]
-		except:
-			pass
-
 		if task_to_restart in task_list:
-			task_table.update({'last_restart_date': current_time, 'paused': False}, (where('task_name') == task_to_restart) & (where('project_name') == current_task_project))
-			click.echo(f'Task: "{task_to_restart}" successfully restarted. Time: {current_time}')
+			is_ended = select_column(task_table.search(where('task_name') == task_to_restart), 'end_date')[0]
+			if is_ended == '':
+				current_task_project = select_column(task_table.search(where('task_name') == task_to_restart), 'project_name')[0]
+			
+				task_table.update({'last_restart_date': current_time, 'paused': False}, (where('task_name') == task_to_restart) & (where('project_name') == current_task_project))
+				click.echo(f'Task: "{task_to_restart}" successfully restarted. Time: {current_time}')
+			else:
+				click.echo('The Task has ended. Please create a new Task.')
 		else:
 			click.echo('That Task does not exist, please try again.')
+
+	elif user_input == 'update_task_name':
+
+		task_list = select_column(task_table.all(), 'task_name')
+		task_command_completer = WordCompleter(task_list, ignore_case=True)
+
+		task_session = PromptSession()
+
+		task_to_update_name = task_session.prompt(
+			'Select Task to Update: ',
+			completer = task_command_completer,
+		)
+
+		name_to_update_to = task_session.prompt(
+			'Update the Task Name: ',
+			completer = task_command_completer,
+			default= task_to_update_name
+		)
+
+		if task_to_update_name in task_list:
+
+			current_task_project = select_column(task_table.search(where('task_name') == task_to_update_name), 'project_name')[0]
+			task_table.update({'task_name': name_to_update_to, 'paused': False}, (where('task_name') == task_to_update_name) & (where('project_name') == current_task_project))
+			click.echo(f'Task "{task_to_update_name}" has been updated to "{name_to_update_to}"')	
+				
+		else:
+			click.echo('That Task does not exist, please try again.')		
 
 	else:
 		click.echo('Not a valid command. Press TAB to view list of possible commands. \n')
